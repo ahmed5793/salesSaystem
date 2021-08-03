0129db40b85d679982c0291c949ce1a0e0fa58c2
ahmed mahmmed abd el twab <Ahmed5793>
2021-08-03 18:31:45 +0200

    mkl,l

--- clothesStore/PL/FormListSuppliers.Designer.cs
+++ clothesStore/PL/FormListSuppliers.Designer.cs
@@ -137,6 +137,7 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "الاصناف";
+            this.Load += new System.EventHandler(this.FormListSuppliers_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
